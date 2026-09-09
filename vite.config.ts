35a5dffb808ddbfacb875853b25643ddea0d878c
Tejaswiniiiiidin <dinkartejaswini@gmail.com>
2026-09-09 20:00:39 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/koraaaa/',
+  base: '/kora/',
   plugins: [react(), tailwindcss()],
 })
